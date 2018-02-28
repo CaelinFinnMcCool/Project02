14716d506c9486817ad8e5d2875f539637d1cf1f
Tim <fauxt@moravian.edu>
2018-02-28 15:53:02 -0500

    add Faux wizard and warrior

---
+++ Project02/FauxWarrior.java
@@ -0,0 +1,36 @@
+package Project02;
+
+public class FauxWarrior extends People {
+    String nation;
+    String tribe;
+    int lifePoints;
+
+    FauxWarrior(String nation, String tribe, int lifePoints){
+        super(nation, tribe, PeopleType.warrior, lifePoints);
+        this.nation = nation;
+        this.tribe = tribe;
+        this.lifePoints = lifePoints;
+    }
+    public int encounterStrategy(People otherPerson){
+        int usedPoints = 0;
+
+        if(this.nation == otherPerson.getNation()){
+            if(this.tribe == otherPerson.getTribe()){
+                if(otherPerson.myDescription.equals("warrior") && otherPerson.getLifePoints() >= this.lifePoints){
+                    usedPoints = -(lifePoints - 2);
+                }
+            }
+        }
+        else{
+            if(otherPerson.getLifePoints() > this.lifePoints && this.lifePoints > 10)
+                usedPoints = 0;
+            else if(lifePoints < 10)
+                usedPoints = this.lifePoints / 2;
+            else {
+                usedPoints = otherPerson.getLifePoints();
+            }
+        }
+
+        return usedPoints;
+    }
+}
---
+++ Project02/FauxWizard.java
@@ -0,0 +1,29 @@
+package Project02;
+
+public class FauxWizard extends People {
+    String nation;
+    String tribe;
+    int lifePoints;
+
+    FauxWizard(String nation, String tribe, int lifePoints)
+    {
+        super(nation, tribe, PeopleType.wizard, lifePoints);
+        this.nation = nation;
+        this.tribe = tribe;
+        this.lifePoints = lifePoints;
+    }
+
+    public int encounterStrategy(Project02.People otherPerson) {
+        int usedPoints = 0;
+
+        if(this.nation == otherPerson.getNation()){
+            if(this.tribe == otherPerson.getTribe()){
+                if(otherPerson.myDescription.equals("warrior") && otherPerson.getLifePoints() > this.lifePoints){
+                    usedPoints = -(lifePoints);
+                }
+            }
+        }
+        return usedPoints;
+    }
+
+}
