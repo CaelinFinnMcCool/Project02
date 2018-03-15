3e4596ea0c1e7711251f29ceb37f17810b9ebeb3
Caelin Finn McCool <mccoolc@moravian.edu>
2018-03-15 16:38:38 -0400

    NischalWarrior.java

--- Project02/NischalWarrior.java
+++ Project02/NischalWarrior.java
@@ -1,32 +1,9 @@
 package Project02;
 
 public class NischalWarrior extends People{
-
     NischalWarrior(String nation, String tribe, int lifePoints) {
         super(nation, tribe, PeopleType.warrior, lifePoints);
         myDescription = "\tNischal Warrior";
-    }
-
-    public int encounterStrategy(People otherPerson)
-    {
-        int lifePoints = 0;
-        if(this.getNation() == otherPerson.getNation()) {
-			return 0;
-
-		}
-		else {
-			if (this.getTribe() != otherPerson.getTribe()) {
-				if (this.getLifePoints() > otherPerson.getLifePoints()) {
-
-					lifePoints = (this.getLifePoints() - otherPerson.getLifePoints()) / 2; //light attack
-				} else {
-					lifePoints = (otherPerson.getLifePoints() - this.getLifePoints()) / 2;
-				}
-			} else {
-				lifePoints = otherPerson.getLifePoints()/2; //big-attack
-			}
-		}
-        return lifePoints;
     }
 
 }
