6c10cb7679d302dad763fbeeacfe77f2ebc78813
Caelin Finn McCool <mccoolc@moravian.edu>
2018-03-15 16:37:53 -0400

    McCoolWizard.java

--- Project02/McCoolWizard.java
+++ Project02/McCoolWizard.java
@@ -3,33 +3,9 @@ package Project02;
 
 public class McCoolWizard extends Project02.People
 {
-    McCoolWizard(String nation, String tribe, int lifePoints)
-    {
-        super(nation, tribe, PeopleType.wizard, lifePoints);
+    McCoolWizard(String nation, String tribe, int lifePoints) {
+        super(nation, tribe, PeopleType.warrior, lifePoints);
         myDescription = "\tMcCool Wizard";
     }
-    
-    public int encounterStrategy(Project02.People otherPerson) {
-        int lifePoints = 0;
-        if (this.getNation() != otherPerson.getNation()) {
-            if (otherPerson.getType() == PeopleType.wizard) {
-                lifePoints = ((this.getLifePoints()));
-            }
-            else {
-                lifePoints = -(this.getLifePoints());
-            }
-        }
-        else
-        {
-            if (otherPerson.getLifePoints() < this.getLifePoints()) {
-                lifePoints = (int) (this.getLifePoints());
-            }
-            else {
-                lifePoints = 0;
-            }
-        }
-        return lifePoints;
-    }
-    
 }
 
