a658224231ef8f0ef1e2bb031dd457804cde175b
Caelin Finn McCool <mccoolc@moravian.edu>
2018-03-15 16:39:01 -0400

    NischalWizard.java

--- Project02/NischalWizard.java
+++ Project02/NischalWizard.java
@@ -1,43 +1,10 @@
 package Project02;
 
 public class NischalWizard extends Project02.People {
-	NischalWizard(String nation, String tribe, int lifePoints) {
-		super(nation, tribe, PeopleType.wizard, lifePoints);
-		myDescription = "\tNischal Wizard";
-
-	}
-
-	public int encounterStrategy(Project02.People otherPerson) {
-		int lifePoints = 0;
-		if (this.getNation() == otherPerson.getNation()) {
-			if (this.getTribe() != otherPerson.getTribe()) {
-				if (this.getLifePoints() < otherPerson.getLifePoints()) {
-
-					lifePoints = (this.getLifePoints() - otherPerson.getLifePoints()) / 2; //heals
-				} else {
-					lifePoints = (otherPerson.getLifePoints() - this.getLifePoints()) / 2;//heals
-				}
-
-			} else {
-				return 0;
-
-			}
-
-
-		} else {
-			if (this.getTribe() != otherPerson.getTribe()) {
-				lifePoints = otherPerson.getLifePoints(); //big attack
-			} else {
-				if (this.getLifePoints() > otherPerson.getLifePoints()) {
-
-					lifePoints = (this.getLifePoints() - otherPerson.getLifePoints()) / 2; //light attack
-				} else {
-					lifePoints = (otherPerson.getLifePoints() - this.getLifePoints()) / 2; //light-attack
-				}
-			}
-		}
-		return lifePoints;
-	}
+    NischalWizard(String nation, String tribe, int lifePoints) {
+        super(nation, tribe, PeopleType.warrior, lifePoints);
+        myDescription = "\tNischal Wizard";
+    }
 }
 
 
