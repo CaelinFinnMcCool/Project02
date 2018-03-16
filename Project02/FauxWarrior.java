238079c8ddf05b108cd34bbdb815c27127a389a6
Caelin Finn McCool <mccoolc@moravian.edu>
2018-03-15 23:17:51 -0400

    FauxWarrior.java

--- Project02/FauxWarrior.java
+++ Project02/FauxWarrior.java
@@ -1,12 +1,12 @@
 package Project02;
 
-public class FauxWarrior extends People {
+public class FauxWarrior extends Project02.People {
     FauxWarrior(String nation, String tribe, int lifePoints) {
         super(nation, tribe, PeopleType.warrior, lifePoints);
         myDescription = "\tFaux Warrior";
     }
 
     public int encounterStrategy(People otherPerson){
-        return FauxWarriorStrategy.encounterStrategy(otherPerson);
+        return 0;
     }
 }
