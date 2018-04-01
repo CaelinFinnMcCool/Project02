f225fff96ab96c8bc58bb40d2515cc428b598c49
Caelin Finn McCool <mccoolc@moravian.edu>
2018-04-01 16:57:15 -0400

    McCoolDisplay.java

--- Project02/McCoolDisplay.java
+++ Project02/McCoolDisplay.java
@@ -1,1 +1,26 @@
+public class McCoolDisplay implements Observer, TribeDisplay {
+    private int totalLifeRemaining;
+    private int tribePopulation;
+    private int warriorsLeft;
+    private int wizardsLeft;
+    
+    public McCoolDisplay(TribeData tribeData){
+        this.tribeData = tribeData;
+        tribeData.registerObserver(this);
+        
+    }
+    public void update(int totalLifeRemaining, int tribePopulation, int warriorsLeft, int wizardsLeft) {
+        this.totalLifeRemaining = totalLifeRemaining;
+        this.tribePopulation = tribePopulation;
+        this.warriorsLeft = warriorsLeft;
+        this.wizardsLeft = wizardsLeft;
+        display();
+    }
+    
+    public void display() {
+        System.out.println(“The McCool tribe has “ + tribePopulation + “ with “ + totalLifeRemaining + “ life points remaining.”);
+        System.out.println(“There are “ + warriorsLeft + “ McCool Warriors left.”);
+        System.out.println(“There are “ + wizardsLeft + “ McCool Wizards left.”);
+    }
+}
 
