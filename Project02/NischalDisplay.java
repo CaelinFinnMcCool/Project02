ba50ef88122a49c0dc9b8e79160d173f5beec542
Nischal Piya <piyan@moravian.edu>
2018-04-01 14:22:12 -0400

    Added NischalDisplay

--- Project02/NischalDisplay.java
+++ Project02/NischalDisplay.java
@@ -1,1 +1,52 @@
 
+
+public class NischalDisplay implements Observer, TribeDisplay{
+    
+    private int NischaltotalLifeRemaining;
+    private int NischaltribePopulation;
+
+    private int NischalwarriorsLeft;
+    private int NischalwizardsLeft;
+    
+    private TribeData tribeData;
+    
+    
+    public NischalDisplay(TribeData tribeData){
+        this.tribeData = tribeData;
+        tribeData.registerObserver(this);
+        
+    }
+    
+    @Override
+    public void update(int NischalWarriorlifePointsremaining , int NischaltribePopulation, int NischalwarriorsLeft, int NischalwizardsLeft)
+    // totalLifeRemaining, tribePopulation, warriorsLeft, wizardsLeft
+    {
+        
+        this.NischaltotalLifeRemaining = NischaltotalLifeRemaining;
+        this.NischaltribePopulation = NischaltribePopulation;
+        this.NischalwarriorsLeft = NischalwarriorsLeft;
+        this.NischalwizardsLeft = NischalwizardsLeft;
+        
+        NischalDisplay();
+        
+    }
+    
+    @Override
+    public void Display(){
+        System.out.println("The total no. of Nischal Warrior left: "+NischalwarriorsLeft);
+        System.out.println("The total no. of Nischal Wizard left: "+NischalwizardsLeft);
+        System.out.println("The total tribe population: "+NischaltribePopulation);
+        System.out.println("The total lifepoints left: "+NischaltotalLifeRemaining);
+
+
+
+        
+        
+        
+        
+        
+    }
+    
+    
+
+}
